34a9b3017557683c80b01906647ff8908bcee10f
White Bull <blueded88@gmail.com>
2023-09-05 12:53:12 -0400

    Testing Google Analytics to the project4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,20 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
-import ReactGa from "react-ga";
+// import ReactGa from "react-ga";
 import Testimonials from "../components/Documents/Testimonials";
 import ExchangeCurrency from "../components/startCurrencyX";
 import StartTutorial from "../components/startTutorial";
 import Footer from "../footer/footer";
 import Navbar from "../navbar/Navbar";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 const Home = () => {
-  const measurementId = import.meta.env.VITE_TAWKTO_GOOGLE_ANALYTICS_ID;
-  ReactGa.initialize(measurementId);
-  
-  useEffect(() => {
-    ReactGa.pageview(window.location.pathname + window.location.search);
-  }, []);
+  // const measurementId = import.meta.env.VITE_TAWKTO_GOOGLE_ANALYTICS_ID;
+  // ReactGa.initialize(measurementId);
+
+  // useEffect(() => {
+  //   ReactGa.pageview(window.location.pathname + window.location.search);
+  // }, []);
   
   return (
     <>
