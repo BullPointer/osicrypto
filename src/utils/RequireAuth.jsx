85c5a2e57d3229ee3cb168b496f023ded86e6fd4
White Bull <blueded88@gmail.com>
2023-09-24 08:56:49 -0400

    Trying to fix an issue with requireAuth

--- src/utils/RequireAuth.jsx
+++ src/utils/RequireAuth.jsx
@@ -4,8 +4,8 @@ import { Navigate, useLocation } from "react-router-dom";
 export const RequireAuth = ({ children }) => {
   const location = useLocation();
   const token = JSON.parse(localStorage.getItem("token"));
-  const expiredTimestamp = token.exp;
-  const currentTimestamp = Math.floor(Date.now() / 1000);
+  const expiredTimestamp = token?.exp;
+  const currentTimestamp = Math.floor(Date.now());
 
   if (children && (!token || expiredTimestamp < currentTimestamp)) {
     localStorage.removeItem("token");
